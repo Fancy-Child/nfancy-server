4a30511fd18f1cdd08b3a3d547524195d5915c01
NAKYUNG LEE <lnk12888@naver.com>
2024-08-19 16:53:56 +0900

    initial commit

---
+++ nfancy/src/main/java/com/entropy/nfancy/NfancyApplication.java
@@ -0,0 +1,13 @@
+package com.entropy.nfancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfancyApplication.class, args);
+	}
+
+}
---
+++ nfancy/src/test/java/com/entropy/nfancy/NfancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entropy.nfancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
